e111b04d3f3c70ea97a450df8beb53ec065f837e
Lius <142863069+noturnachs@users.noreply.github.com>
2024-09-12 14:41:18 +0800

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -120,12 +120,12 @@ function Home() {
             }`}
           >
             <Link to="/donate">
-              <button className="relative py-2 px-6 w-full md:w-max text-white text-base font-bold rounded-full overflow-hidden bg-blue-500 transition-all duration-700 ease-in-out hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0">
+              <button className="relative py-2 px-6 w-full md:w-max text-white text-base font-bold rounded-full overflow-hidden bg-blue-500 transition-all duration-700 ease-in-out hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-green-500 before:to-green-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0 bg-green-600">
                 Donate Now
               </button>
             </Link>
             <Link to="/programs">
-              <button className="relative py-2 px-6 w-full md:w-auto text-black text-base font-bold rounded-full overflow-hidden bg-blue-50 transition-all duration-500 ease-in-out hover:scale-110 hover:shadow-xl before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#ff7e5f] before:to-[#feb47b] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0">
+              <button className="relative py-2 px-6 w-full md:w-auto text-white text-base font-bold rounded-full overflow-hidden bg-blue-50 transition-all duration-500 ease-in-out hover:scale-110 hover:shadow-xl before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-300 before:to-blue-700 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0 bg-blue-500">
                 <span className="flex flex-row items-center justify-center">
                   <IoIosInformationCircleOutline size={20} /> &nbsp;Learn More
                 </span>
@@ -136,7 +136,7 @@ function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <button className="relative py-2 px-6 w-full md:w-auto text-black text-base font-bold rounded-full overflow-hidden bg-blue-50 transition-all duration-500 ease-in-out hover:scale-110 hover:shadow-xl before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#ff7e5f] before:to-[#feb47b] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0">
+              <button className="relative py-2 px-6 w-full md:w-auto text-white text-base font-bold rounded-full overflow-hidden bg-blue-50 transition-all duration-500 ease-in-out hover:scale-110 hover:shadow-xl before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-300 before:to-blue-700 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0 bg-blue-500">
                 <span className="flex flex-row items-center justify-center">
                   <FaRegNewspaper size={20} /> &nbsp;Our Newsletter
                 </span>
